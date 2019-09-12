7d2f0d1598c9b96586760ca8d326b11b7f4527f0
Saif <saifeddinerashed@icloud.com>
2019-09-13 00:17:29 +0200

    styling improvements contact & roboto font added

--- view/header.php
+++ view/header.php
@@ -22,6 +22,8 @@ session_start();
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900,900i&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" type="text/css" href="assets/css/grids.css">
     <link rel="stylesheet" type="text/css" href="assets/css/main.css">
 </head>
