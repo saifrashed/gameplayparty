6319a0bbd0bbd0eba64b801eda0d9da14c977b9d
Brandon030 <brandon.louw@hotmail.nl>
2019-10-17 01:43:25 +0200

    footer aangepast
    links css veranderd.
    
    BELANGRIJK: kijken hoe het best aangeven dat het een button is en css duidelijk maken hoe en op welke pagina je zit.

--- view/footer.php
+++ view/footer.php
@@ -8,16 +8,18 @@
             <hr class="w-100 clearfix d-md-none">
 
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">Links</h6>
-                <p>
-                    <a href="?op=home">Home</a>
-                </p>
-                <p>
-                    <a href="?op=reservations">Reserveren</a>
-                </p>
-                <p>
-                    <a href="?op=contact">Contact</a>
-                </p>
+                <h6 class="text text-uppercase mb-4 font-weight-bold">Links</h6>
+                <div style="">
+                    <p>
+                        <a href="?op=home" style="color: black;">Home</a>
+                    </p>
+                    <p>
+                        <a href="?op=reservations" style="color: black;">Reserveren</a>
+                    </p>
+                    <p>
+                        <a href="?op=contact" style="color: black;">Contact</a>
+                    </p>
+                </div>                
             </div>
 
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
