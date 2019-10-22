7d78fdc9f7edd49ad3d1408057b86f81e921838b
Saif <saifeddinerashed@icloud.com>
2019-10-22 17:21:16 +0200

    fixed footer

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,32 @@
 </div>
 
+
+<footer class="page-footer font-small mdb-color pt-4" style="background-color:#43a047;">
+    <div class="container">
+        <div class="row text-center text-md-left">
+
+            <div class="col-md-1 col-lg-1 col-xl-1 mx-auto mt-3">
+                <h6 class="text text-uppercase mb-4 font-weight-bold">Links</h6>
+            </div>
+
+            <div class="col-md-1 col-lg-1 col-xl-1 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">Policies</h6>
+                <a href="#" style="color: black;">Cookies</a>
+                <a href="#" style="color: black;">privacy</a>
+                <a href="#" style="color: black;">refund</a>
+                <a href="#" style="color: black;">terms</a>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                    <i class="fas fa-envelope mr-3"></i> jack.jones@gameplayparty.nl</p>
+                    <i class="fas fa-phone mr-3"></i> (656)-976-4980</p>
+            </div>
+
+        </div>
+    </div>
+</footer>
+
 <!-- Scripts -->
 <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"
