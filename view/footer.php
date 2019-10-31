eecdf20eca1462e513deb97b54e5d29ff8845a91
Brandon030 <brandon.louw@hotmail.nl>
2019-10-31 20:11:39 +0100

    footer aangepast

--- view/footer.php
+++ view/footer.php
@@ -1,30 +1,50 @@
 </div>
 
+<footer class="page-footer font-small mdb-color pt-4" style="background-color:#43a047;">
 
-<footer class="main-footer">
-    <div class="container">
-        <div class="row text-left">
+    <div class="container text-center text-md-left">
+
+        <div class="row text-center text-md-left mt-3 pb-3">
+            <hr class="w-100 clearfix d-md-none">
 
             <div class="col-md-1 col-lg-1 col-xl-1 mx-auto mt-3">
-                <h6 class="text text-uppercase mb-4 font-weight-bold">Links</h6>
+                <h6 class="text text-uppercase mb-4 font-weight-bold">Paginas</h6>
+                
+                <div>
+                    <p>
+                        <a href="?op=home" style="color: black;">Home</a>
+                    </p>
+                    <p>
+                        <a href="?op=reservations" style="color: black;">Reserveren</a>
+                    </p>
+                    <p>
+                        <a href="?op=contact" style="color: black;">Contact</a>
+                    </p>
+                </div>                
             </div>
 
             <div class="col-md-1 col-lg-1 col-xl-1 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">Policies</h6>
-                <ul>
-                    <li><a href="#">Cookies</a></li>
-                    <li><a href="#">privacy</a></li>
-                    <li><a href="#">refund</a></li>
-                    <li><a href="#">terms</a></li>
-                </ul>
+                <h6 class="text-uppercase mb-4 font-weight-bold">Policys</h6>
+                <p>
+                    <a href="?op=cookies" style="color: black;">Cookies</a>
+                <p>
+                    <a href="?op=privacy" style="color: black;">privacy</a>
+                </p>
+                <p>
+                    <a href="?op=refund" style="color: black;">refund</a>
+                </p>
+                <p>
+                    <a href="?op=terms" style="color: black;">terms</a>
+                </p>
             </div>
 
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <div class="col-md-2 col-lg-3 col-xl-2 mx-auto mt-3">
                 <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                <i class="fas fa-envelope mr-3"></i> jack.jones@gameplayparty.nl</p>
-                <i class="fas fa-phone mr-3"></i> (656)-976-4980</p>
+                <p>
+                    <i class="fas fa-envelope mr-3"></i> jack.jones@gameplayparty.nl</p>
+                <p>
+                    <i class="fas fa-phone mr-3"></i> (656)-976-4980</p>
             </div>
-
         </div>
     </div>
 </footer>
